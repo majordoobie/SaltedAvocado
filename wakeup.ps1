63e6991ccc641cbafb8edbb5e59507bfd55fa7f4
majordoobie <sgtmajordoobie@live.com>
2018-12-28 01:36:07 -0500

    Commit

---
+++ wakeup.ps1
@@ -0,0 +1,9 @@
+﻿$somefuckingvariable = New-Object -com "Wscript.Shell"
+$i = 0
+Write-Host "Just leave me running, minimize the window." -ForegroundColor Red
+while($true){
+    $i ++ 
+    Start-Sleep 180
+    $somefuckingvariable.sendkeys("{SCROLLLOCK}")
+    Write-Host "Wake the fuck up, please: $i" -ForegroundColor Magenta
+}
